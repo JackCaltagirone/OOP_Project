9bbb061fa410d72d8d76fe676df11a7ff33e1016
Jack <jackcaltagirone1996@gmail.com>
2019-01-05 16:24:41 +0000

    Initial commit

---
+++ src/ie/gmit/sw/BlockingQueue.java
@@ -0,0 +1,11 @@
+package ie.gmit.sw;
+
+public class BlockingQueue {
+	
+	public void addQueue(){
+
+	
+	
+	}
+
+}
---
+++ src/ie/gmit/sw/Runner.java
@@ -0,0 +1,20 @@
+package ie.gmit.sw;
+
+import java.io.IOException;
+
+public class Runner {
+
+	/**  
+	 * @author g00349976 - Jack Caltagirone
+	 * 
+	 * this is the runner class. it starts the program from the console
+	 * 
+	 * It calls the parsing class where the files are selected and parsed into shingles
+	 * @throws InterruptedException 
+	 * */
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		new Menu().UI();
+	}//main
+
+}
